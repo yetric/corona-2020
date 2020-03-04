bfbebea9d8598bec87253ca27b7f89f28e56efbe
Mattias Hising <hising@gmail.com>
2020-03-04 20:13:27 +0100

    Chart with corona spread in sweden

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import {Line} from 'react-chartjs-2';
 import './App.css';
+import Papa from "papaparse";
+
+const headerCountry = "";
+
+const REGION_IDX = 0;
+const COUNTRY_IDX = 1;
+const LAT_IDX = 2;
+const LNG_IDX = 3;
+const DATA_START_IDX = 4;
+
+const confirmedCasesCSV = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+//const confirmedDeathsCSV = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv";
+//const confirmedRecoveredCSV = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv";
 
 function App() {
+  const [chartData, setChartData] = useState({});
+  const getCountry = (dataset: any[], countryName: string) => {
+    for (let i = 0; i < dataset.length; i++) {
+      const row = dataset[i];
+      const country = row[COUNTRY_IDX];
+      if (country === countryName) {
+        return row.splice(DATA_START_IDX).map((nr: string) => parseInt(nr));
+      }
+    }
+  };
+
+  const getLabels = (dataset: any[]) => {
+    return dataset[0].splice(DATA_START_IDX);
+  };
+
+  useEffect(() => {
+    loadCases();
+  }, []);
+
+  const loadCases = async () => {
+    Papa.parse(confirmedCasesCSV, {
+      download: true,
+      header: false,
+      complete: function(results) {
+        const swedenData = getCountry(results.data, "Sweden");
+        const chartDataset = {
+          labels: getLabels(results.data),
+          datasets: [
+            {
+              fill: false,
+              lineTension: 0.1,
+              label: "Confirmed Cases Sweden",
+              data: swedenData,
+              backgroundColor: 'rgba(75,192,192,0.4)',
+              borderColor: 'rgba(75,192,192,1)',
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              pointBorderColor: 'rgba(75,192,192,1)',
+              pointBackgroundColor: '#fff',
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+              pointHoverBorderColor: 'rgba(220,220,220,1)',
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+            }
+          ]
+        };
+        setChartData(chartDataset);
+      }
+    });
+
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"chart"}>
+      <h2>Data on Corona in Sweden</h2>
+      <Line data={chartData} />
+      <p><a href={"https://github.com/CSSEGISandData/COVID-19"}>Data Source</a> - Graph by <a href={"https://yetric.com"}>Yetric AB</a> - Pull Requests Welcome here</p>
     </div>
   );
 }
