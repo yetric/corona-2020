45c4493c581903f2e03cd64781523231d67a3ecf
Mattias Hising <hising@gmail.com>
2020-03-08 17:13:47 +0100

    Added ma func

---
+++ src/core/stats.js
@@ -0,0 +1,48 @@
+export const isNumber = (subject) => typeof subject === "number";
+export const ma = (data, size) => {
+    const length = data.length;
+
+    if (!size) {
+        return data.reduce((a, b) => a + b) / length;
+    }
+
+    if (size <= 1) {
+        return data.slice();
+    }
+
+    if (size > length) {
+        return Array(length);
+    }
+
+    const prepare = size - 1;
+    const ret = [];
+    let sum = 0;
+    let i = 0;
+    let counter = 0;
+    let datum;
+
+    for (; i < length && counter < prepare; i++) {
+        datum = data[i];
+
+        if (isNumber(datum)) {
+            sum += datum;
+            counter++;
+        }
+    }
+
+    for (; i < length; i++) {
+        datum = data[i];
+
+        if (isNumber(datum)) {
+            sum += datum;
+        }
+
+        if (isNumber(data[i - size])) {
+            sum -= data[i - size];
+        }
+
+        ret[i] = sum / size;
+    }
+
+    return ret;
+};
