71ef18cf29c4472869e6e2381757549949f97e70
Mattias Hising <hising@gmail.com>
2020-03-13 15:17:08 +0100

    Fixed front page

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,20 @@ const App = () => {
 
             <Switch>
                 <Route path={"/:country"} component={Home} />
+                <Route path={"/"}>
+                    <ul className={"list-group"}>
+                        <li><Link to={"/18"}>Sweden</Link></li>
+                        <li><Link to={"/40"}>Norway</Link></li>
+                        <li><Link to={"/174"}>Denmark</Link></li>
+                        <li><Link to={"/13"}>Finland</Link></li>
+                        <li><Link to={"/46"}>Iceland</Link></li>
+                        <li><Link to={"/12"}>Germany</Link></li>
+                        <li><Link to={"/17"}>Italy</Link></li>
+                        <li><Link to={"/19"}>Spain</Link></li>
+                        <li><Link to={"/160"}>France</Link></li>
+                        <li><Link to={"/173"}>UK</Link></li>
+                    </ul>
+                </Route>
             </Switch>
 
 
