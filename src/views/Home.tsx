a0e519018c24391f58649e908b5407258a10c8ea
Mattias Hising <hising@gmail.com>
2020-03-17 14:56:33 +0100

    Rearranged Home.tsx so we can start showing map on front

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -8,80 +8,92 @@ export const Home = observer(() => {
         document.title = "Corona (Covid-19) Data 2020 - Graph and Table - CoronaData.se";
     });
     return (
-        <div>
-            <ListFavs />
-            <div className="card">
-                <div className="card-header">Advice</div>
-                <div className="card-body">
-                    <ol className={"numbered"}>
-                        <li>Stay home if you are sick</li>
-                        <li>Wash your hands often</li>
-                        <li>Cough and sneeze in your arm</li>
-                        <li>Avoid touching your face</li>
-                        <li>If you use hand sanitizer use 60%+</li>
-                        <li>Avoid people with symptoms</li>
-                        <li>Do not visit people in risk groups</li>
-                        <li>Stay safe</li>
-                    </ol>
+        <>
+            <div className={"row"}>
+                <div className="col">
+                    <ListFavs />
                 </div>
-                <div className="card-footer">
-                    <Share />
-                </div>
-            </div>
-
-            <ul className={"list-group"}>
-                <li className={"list-header"}>Swedish links</li>
-                <li>
-                    <a
-                        target={"_blank"}
-                        rel={"noopener noreferrer"}
-                        href={
-                            "https://www.1177.se/sjukdomar--besvar/infektioner/ovanliga-infektioner/covid-19-coronavirus/"
-                        }>
-                        1177
-                    </a>
-                </li>
-                <li>
-                    <a
-                        target={"_blank"}
-                        rel={"noopener noreferrer"}
-                        href={
-                            "https://www.msb.se/sv/aktuellt/pagaende-handelser-och-insatser/msbs-arbete-med-anledning-av-coronaviruset/"
-                        }>
-                        Myndigheten för samhällsskydd och beredskap (MSB)
-                    </a>
-                </li>
-                <li>
-                    <a
-                        target={"_blank"}
-                        rel={"noopener noreferrer"}
-                        href={
-                            "https://www.regeringen.se/regeringens-politik/regeringens-arbete-med-anledning-av-nya-coronaviruset/"
-                        }>
-                        Regeringen
-                    </a>
-                </li>
-                <li>
-                    <a
-                        target={"_blank"}
-                        rel={"noopener noreferrer"}
-                        href={
-                            "https://www.folkhalsomyndigheten.se/smittskydd-beredskap/utbrott/aktuella-utbrott/covid-19/"
-                        }>
-                        Folkhälsomyndigheten
-                    </a>
-                </li>
-            </ul>
-
-            <div className="card">
-                <div className="card-header">About CoronaData.se</div>
-                <div className="card-body">
-                    An attempt to make data on the Covid-19 outbreak a bit more accessible and easy to overview on
-                    country levels. Data is based on reports to Johns Hopkins University and are updated daily.
-                    CoronaData.se is hosted, built and updated by <a href={"https://yetric.com"}>Yetric AB</a> who are
-                    experts in data-driven product development.
+                <div className="col">
+                    <ul className={"list-group"}>
+                        <li className={"list-header"}>Swedish links</li>
+                        <li>
+                            <a
+                                target={"_blank"}
+                                rel={"noopener noreferrer"}
+                                href={
+                                    "https://www.1177.se/sjukdomar--besvar/infektioner/ovanliga-infektioner/covid-19-coronavirus/"
+                                }>
+                                1177
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                target={"_blank"}
+                                rel={"noopener noreferrer"}
+                                href={
+                                    "https://www.msb.se/sv/aktuellt/pagaende-handelser-och-insatser/msbs-arbete-med-anledning-av-coronaviruset/"
+                                }>
+                                Myndigheten för samhällsskydd och beredskap (MSB)
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                target={"_blank"}
+                                rel={"noopener noreferrer"}
+                                href={
+                                    "https://www.regeringen.se/regeringens-politik/regeringens-arbete-med-anledning-av-nya-coronaviruset/"
+                                }>
+                                Regeringen
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                target={"_blank"}
+                                rel={"noopener noreferrer"}
+                                href={
+                                    "https://www.folkhalsomyndigheten.se/smittskydd-beredskap/utbrott/aktuella-utbrott/covid-19/"
+                                }>
+                                Folkhälsomyndigheten
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-        </div>
+            <div className={"row"}>
+                <div className="col">
+                    <div className="card">
+                        <div className="card-header">Advice</div>
+                        <div className="card-body">
+                            <ol className={"numbered"}>
+                                <li>Stay home if you are sick</li>
+                                <li>Wash your hands often</li>
+                                <li>Cough and sneeze in your arm</li>
+                                <li>Avoid touching your face</li>
+                                <li>If you use hand sanitizer use 60%+</li>
+                                <li>Avoid people with symptoms</li>
+                                <li>Do not visit people in risk groups</li>
+                                <li>Stay safe</li>
+                            </ol>
+                        </div>
+                        <div className="card-footer">
+                            <Share />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col">
+                    <div className="card">
+                        <div className="card-header">About CoronaData.se</div>
+                        <div className="card-body">
+                            An attempt to make data on the Covid-19 outbreak a bit more accessible and easy to overview
+                            on country levels. Data is based on reports to Johns Hopkins University and are updated
+                            daily. CoronaData.se is hosted, built and updated by{" "}
+                            <a href={"https://yetric.com"}>Yetric AB</a> who are experts in data-driven product
+                            development.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 });
