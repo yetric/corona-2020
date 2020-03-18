affc76d1fd5d29ad9c000f43c4f26c89ebbd9493
Mattias Hising <hising@gmail.com>
2020-03-18 18:27:20 +0100

    Added link to sll

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -4,9 +4,16 @@ export const Links = () => (
     <ul className={"list-group"}>
         <li className={"list-header"}>Swedish links</li>
         <li>
+            <a target={"_blank"} rel="noopener noreferrer" href={"https://corona.sll.se/"}>
+                Behöver jag ringa vården?
+                <br />
+                Självskattning (Region Stockholm) <span className={"label"}>New</span>
+            </a>
+        </li>
+        <li>
             <a
                 target={"_blank"}
-                rel={"noopener noreferrer"}
+                rel="noopener noreferrer"
                 href={"https://www.1177.se/sjukdomar--besvar/infektioner/ovanliga-infektioner/covid-19-coronavirus/"}>
                 1177
             </a>
@@ -14,7 +21,7 @@ export const Links = () => (
         <li>
             <a
                 target={"_blank"}
-                rel={"noopener noreferrer"}
+                rel="noopener noreferrer"
                 href={
                     "https://www.msb.se/sv/aktuellt/pagaende-handelser-och-insatser/msbs-arbete-med-anledning-av-coronaviruset/"
                 }>
@@ -24,7 +31,7 @@ export const Links = () => (
         <li>
             <a
                 target={"_blank"}
-                rel={"noopener noreferrer"}
+                rel="noopener noreferrer"
                 href={
                     "https://www.regeringen.se/regeringens-politik/regeringens-arbete-med-anledning-av-nya-coronaviruset/"
                 }>
@@ -34,7 +41,7 @@ export const Links = () => (
         <li>
             <a
                 target={"_blank"}
-                rel={"noopener noreferrer"}
+                rel="noopener noreferrer"
                 href={"https://www.folkhalsomyndigheten.se/smittskydd-beredskap/utbrott/aktuella-utbrott/covid-19/"}>
                 Folkhälsomyndigheten
             </a>
