092e4fa6e45a418e3ba8085cf7068f797ce3609f
Mattias Hising <hising@gmail.com>
2020-03-19 13:06:12 +0100

    Include ListFavs in Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,6 +3,7 @@ import "./Dashboard.css";
 import { CoronaMap } from "./CoronaMap";
 import { MapStore } from "../stores/MapStore";
 import { observer } from "mobx-react";
+import { ListFavs } from "./ListFavs";
 
 const mapStore = new MapStore();
 
@@ -20,59 +21,7 @@ export const Dashboard = observer(() => {
                 <CoronaMap lat={46.8182} lng={8.2275} zoom={2} geoJSON={mapStore.geometry} />
             </div>
             <div className="col col-1" id={"dashboard-countries"}>
-                <ul className={"list-group"}>
-                    <li>
-                        <a
-                            href={"#sdfsdfsdf"}
-                            onClick={(event) => {
-                                event.preventDefault();
-                                mapStore.load(91);
-                            }}>
-                            United States
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href={"#"}
-                            onClick={(event) => {
-                                event.preventDefault();
-                                mapStore.load(96);
-                            }}>
-                            China
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href={"#"}
-                            onClick={(event) => {
-                                event.preventDefault();
-                                mapStore.load(16);
-                            }}>
-                            Sweden
-                        </a>
-                    </li>
-                    <li>
-                        <a href={"#"}>sdfsdf</a>
-                    </li>
-                    <li>
-                        <a href={"#"}>sdfsdf</a>
-                    </li>
-                    <li>
-                        <a href={"#"}>sdfsdf</a>
-                    </li>
-                    <li>
-                        <a href={"#"}>sdfsdf</a>
-                    </li>
-                    <li>
-                        <a href={"#"}>sdfsdf</a>
-                    </li>
-                    <li>
-                        <a href={"#"}>sdfsdf</a>
-                    </li>
-                    <li>
-                        <a href={"#"}>sdfsdf</a>
-                    </li>
-                </ul>
+                <ListFavs />
             </div>
         </div>
     );
