782503b29bf6c67cad9c665c7185efdf326fba69
Mattias Hising <hising@gmail.com>
2020-03-21 22:22:28 +0100

    Added some Advice.tsx

--- src/components/Advice.tsx
+++ src/components/Advice.tsx
@@ -6,14 +6,16 @@ export const Advice = () => (
         <div className="card-header">Advice</div>
         <div className="card-body">
             <ol className={"numbered"}>
-                <li>Stay home if you are sick</li>
+                <li>Stay home if you feel sick</li>
                 <li>Wash your hands often</li>
                 <li>Cough and sneeze in your arm</li>
                 <li>Avoid touching your face</li>
                 <li>If you use hand sanitizer use 60%+</li>
-                <li>Avoid people with symptoms</li>
+                <li>Keep distance to other people</li>
                 <li>Do not visit people in risk groups</li>
-                <li>Stay safe</li>
+                <li>Follow reliable news sources</li>
+                <li>Do not stockpile toilet paper ;)</li>
+                <li>Be smart, stay safe</li>
             </ol>
         </div>
         <div className="card-footer">
