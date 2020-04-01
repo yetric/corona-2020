788d23e0f0a45ec11fee5d52b2e665580dab3fd4
Mattias Hising <hising@gmail.com>
2020-04-01 16:49:54 +0200

    Tweak of chart colors.ts

--- src/core/colors.ts
+++ src/core/colors.ts
@@ -1,4 +1,4 @@
-export const colors = ["#81D4FA", "#F44336", "#9CCC65", "#FDD835"];
+export const colors = ["#29B6F6", "#F44336", "#66BB6A", "#FDD835"];
 
 export const blue = colors[0];
 export const red = colors[1];
