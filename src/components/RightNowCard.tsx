8e550b4dffb8852e486c58b6a44a138e84900d83
Mattias Hising <hising@gmail.com>
2020-04-05 22:06:48 +0200

    Added active

--- src/components/RightNowCard.tsx
+++ src/components/RightNowCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { DataStore } from "../stores/DataStore";
+import { relativeToPercentage } from "../core/functions";
 
 interface RightNowCardProps {
     dataStore: DataStore;
@@ -13,21 +14,36 @@ export const RightNowCard = ({ dataStore, casesPerHundraK }: RightNowCardProps) 
             <dl>
                 <dt>Confirmed</dt>
                 <dd className={"confirmed"}>
-                    {dataStore.data?.confirmed.count} <small>{dataStore.data?.confirmed.date}</small>
+                    {dataStore.data?.confirmed.count.toLocaleString("sv-se")}{" "}
+                    <small>{dataStore.data?.confirmed.date}</small>
                 </dd>
 
                 <dt>Deaths</dt>
                 <dd className={"deaths"}>
-                    {dataStore.data?.deaths.count} <small>{dataStore.data?.active.deathRate}%</small>
+                    {dataStore.data?.deaths.count.toLocaleString("sv-se")}{" "}
+                    <small>{dataStore.data?.active.deathRate}%</small>
                 </dd>
 
                 <dt>Recovered</dt>
                 <dd className={"recovered"}>
-                    {dataStore.data?.recovered.count} <small>{dataStore.data?.active.recoveryRate}%</small>
+                    {dataStore.data?.recovered.count.toLocaleString("sv-se")}{" "}
+                    <small>{dataStore.data?.active.recoveryRate}%</small>
+                </dd>
+
+                <dt>Active</dt>
+                <dd className={"active"}>
+                    {dataStore.data?.active.count.toLocaleString("sv-se")}{" "}
+                    <small>
+                        {dataStore.data &&
+                            dataStore.data.active &&
+                            relativeToPercentage(dataStore.data.active.count / dataStore.data.confirmed.count)}
+                    </small>
                 </dd>
 
                 <dt>Incidens</dt>
-                <dd className={"muted"}>{casesPerHundraK}</dd>
+                <dd className={"muted"}>
+                    {casesPerHundraK} <small>cases / 100K</small>
+                </dd>
             </dl>
         </div>
     </div>
