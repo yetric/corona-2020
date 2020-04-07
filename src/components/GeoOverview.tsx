3c5a07e6c5d04df3794bb5ed9e27c40456cb78be
Mattias Hising <hising@gmail.com>
2020-04-07 15:31:49 +0200

    Rearrange cards on GeoOverview.tsx

--- src/components/GeoOverview.tsx
+++ src/components/GeoOverview.tsx
@@ -10,18 +10,23 @@ export const GeoOverview = observer(() => {
         <>
             <div className={"cards"}>
                 <ReportCard report={"world"} store={new ReportStore()} />
-                <ReportCard report={"US"} store={new ReportStore()} />
                 <ReportCard report={"continent:Europe"} store={new ReportStore()} />
+                <ReportCard report={"Sweden"} store={new ReportStore()} />
+            </div>
+            <div className={"cards"}>
+                <ReportCard report={"United Kingdom"} store={new ReportStore()} />
+                <ReportCard report={"Germany"} store={new ReportStore()} />
+                <ReportCard report={"France"} store={new ReportStore()} />
             </div>
             <div className="cards">
-                <ReportCard report={"Sweden"} store={new ReportStore()} />
-                <ReportCard report={"region:Nordic Countries"} store={new ReportStore()} />
+                <ReportCard report={"US"} store={new ReportStore()} />
+                <ReportCard report={"Spain"} store={new ReportStore()} />
                 <ReportCard report={"Italy"} store={new ReportStore()} />
             </div>
             <div className="cards">
-                <ReportCard report={"France"} store={new ReportStore()} />
-                <ReportCard report={"Spain"} store={new ReportStore()} />
-                <ReportCard report={"Germany"} store={new ReportStore()} />
+                <ReportCard report={"region:Nordic Countries"} store={new ReportStore()} />
+                <ReportCard report={"Norway"} store={new ReportStore()} />
+                <ReportCard report={"Denmark"} store={new ReportStore()} />
             </div>
         </>
     );
