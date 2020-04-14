e32278758c8c4f7290e6be3df1e190dd9a5a650b
Mattias Hising <hising@gmail.com>
2020-04-14 17:38:15 +0200

    remove unused import

--- src/components/Report.tsx
+++ src/components/Report.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useState } from "react";
+import React, { memo } from "react";
 import { Line } from "react-chartjs-2";
 import { ReportInterface } from "../stores/ReportStore";
 import { createDataset } from "../core/helpers";
