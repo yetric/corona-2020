445e3d21051f2377c8093b6040cd5ec06bf0d9b6
Mattias Hising <hising@gmail.com>
2020-04-17 11:02:45 +0200

    register serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
+serviceWorker.register();
 ReactDOM.render(<App />, document.getElementById("root"));
