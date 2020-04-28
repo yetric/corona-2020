70b76302b2b025e7603c5eea4859e657c73ef7fd
Mattias Hising <hising@gmail.com>
2020-04-28 17:16:33 +0200

    Two per row

--- src/components/GeoOverview.tsx
+++ src/components/GeoOverview.tsx
@@ -9,32 +9,36 @@ export const GeoOverview = observer(() => {
             <div className={"cards"}>
                 <ReportCard report={"world"} store={new ReportStore()} />
                 <ReportCard report={"continent:Europe"} store={new ReportStore()} />
-                <ReportCard report={"Sweden"} store={new ReportStore()} />
             </div>
             <div className={"cards"}>
+                <ReportCard report={"Sweden"} store={new ReportStore()} />
                 <ReportCard report={"United Kingdom"} store={new ReportStore()} />
+            </div>
+            <div className={"cards"}>
                 <ReportCard report={"Germany"} store={new ReportStore()} />
                 <ReportCard report={"France"} store={new ReportStore()} />
             </div>
             <div className="cards">
                 <ReportCard report={"US"} store={new ReportStore()} />
                 <ReportCard report={"Spain"} store={new ReportStore()} />
-                <ReportCard report={"Italy"} store={new ReportStore()} />
             </div>
-
-            <div className="cards">
+            <div className={"cards"}>
+                <ReportCard report={"Italy"} store={new ReportStore()} />
                 <ReportCard report={"Korea, South"} store={new ReportStore()} />
+            </div>
+            <div className={"cards"}>
                 <ReportCard report={"Iran"} store={new ReportStore()} />
                 <ReportCard report={"Brazil"} store={new ReportStore()} />
             </div>
-
             <div className="cards">
                 <ReportCard report={"Finland"} store={new ReportStore()} />
                 <ReportCard report={"Norway"} store={new ReportStore()} />
-                <ReportCard report={"Denmark"} store={new ReportStore()} />
             </div>
-            <div className="cards">
+            <div className={"cards"}>
+                <ReportCard report={"Denmark"} store={new ReportStore()} />
                 <ReportCard report={"China"} store={new ReportStore()} />
+            </div>
+            <div className={"cards"}>
                 <ReportCard report={"Japan"} store={new ReportStore()} />
                 <ReportCard report={"Australia"} store={new ReportStore()} />
             </div>
@@ -42,7 +46,10 @@ export const GeoOverview = observer(() => {
             <div className="cards">
                 <ReportCard report={"Russia"} store={new ReportStore()} />
                 <ReportCard report={"Netherlands"} store={new ReportStore()} />
+            </div>
+            <div className={"cards"}>
                 <ReportCard report={"Belgium"} store={new ReportStore()} />
+                <ReportCard report={"Canada"} store={new ReportStore()} />
             </div>
         </>
     );
