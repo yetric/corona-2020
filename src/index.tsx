cf92a71185534310907efc6c2a32dd1f38451151
Mattias Hising <hising@gmail.com>
2020-05-03 12:36:27 +0200

    Add pwa/web class to body instead after rendering

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { isInStandaloneMode } from "./core/helpers";
 
 serviceWorker.unregister();
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"), () => {
+    document.body.classList.add(isInStandaloneMode() ? "pwa" : "web");
+});
