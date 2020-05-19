4bd6b505b79a4a21cb23744fef21f1b28e2478ec
Mattias Hising <hising@gmail.com>
2020-05-19 19:39:09 +0200

    Link to route for World.tsx

--- src/components/GeoOverview.tsx
+++ src/components/GeoOverview.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { observer } from "mobx-react";
 import { ReportStore } from "../stores/ReportStore";
 import { ReportCard } from "./ReportCard";
+import { Link } from "react-router-dom";
+import { Globe } from "react-feather";
 
 export const GeoOverview = observer(() => {
     return (
@@ -10,10 +12,18 @@ export const GeoOverview = observer(() => {
                 <ReportCard report={"world"} store={new ReportStore()} />
                 <ReportCard report={"continent:Europe"} store={new ReportStore()} />
             </div>
+            <ul className={"pills"}>
+                <li>
+                    <Link to={"/world"}>
+                        <Globe size={18} /> More on World Growth and Doubling Speeds
+                    </Link>
+                </li>
+            </ul>
             <div className={"cards"}>
                 <ReportCard report={"Sweden"} store={new ReportStore()} />
                 <ReportCard report={"United Kingdom"} store={new ReportStore()} />
             </div>
+
             <div className={"cards"}>
                 <ReportCard report={"Germany"} store={new ReportStore()} />
                 <ReportCard report={"France"} store={new ReportStore()} />
