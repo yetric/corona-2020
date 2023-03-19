2318fa8504c33a730fee0cfdcf6bc96f848d21a5
Mattias Hising <hising@gmail.com>
2023-03-19 11:26:42 +0100

    Discontinue site ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,18 @@
-import React, { Suspense, lazy, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link, useLocation, withRouter } from "react-router-dom";
 import Analytics from "react-router-ga";
 
 import "./core/toaster";
 
 import { BarChart2 } from "react-feather";
-import { LoadingView } from "./views/Loading";
 import Home from "./views/Home";
 import { observer } from "mobx-react";
 import { isInStandaloneMode } from "./core/helpers";
-import { ContextTools } from "./components/ContextTools";
 import { Modal } from "./components/core/Modal";
 import { Button } from "./components/Button";
 import { getCookie, setCookie } from "./core/utils";
 
-const Geo = lazy(() => import("./views/Geo"));
-const Continent = lazy(() => import("./views/Continent"));
-const Region = lazy(() => import("./views/Region"));
-const Government = lazy(() => import("./views/Government"));
-const Expectancy = lazy(() => import("./views/Expectancy"));
-const Country = lazy(() => import("./views/Country"));
-const About = lazy(() => import("./views/About"));
-const World = lazy(() => import("./views/World"));
-
 const NoMatchPage = () => <div>File not found</div>;
-
-const WaitingComponent = (Component: any) => {
-    return (props: any) => (
-        <Suspense fallback={<LoadingView />}>
-            <Component {...props} />
-        </Suspense>
-    );
-};
 
 export const ScrollToTop = withRouter(() => {
     const { pathname } = useLocation();
@@ -75,26 +56,15 @@ const App = observer(() => {
                     </div>
                     <div className={"chart"}>
                         <Switch>
-                            <Route exact path={"/about"} component={WaitingComponent(About)} />
-                            <Route exact path={"/world"} component={WaitingComponent(World)} />
-                            <Route exact path={"/continent/:continent"} component={WaitingComponent(Continent)} />
-                            <Route exact path={"/region/:region"} component={WaitingComponent(Region)} />
-                            <Route exact path={"/government/:government"} component={WaitingComponent(Government)} />
-                            <Route exact path={"/expectancy/:expectancy"} component={WaitingComponent(Expectancy)} />
-                            <Route exact path={"/report/:country"} component={WaitingComponent(Country)} />
-                            <Route exact path={"/:country"} component={WaitingComponent(Geo)} />
                             <Route exact path={"/"} component={() => <Home />} />
                             <Route component={NoMatchPage} />
                         </Switch>
 
                         <p className={"footer"}>
-                            <a href={"https://github.com/CSSEGISandData/COVID-19"}>Data Source</a> - Graph by{" "}
-                            <a href={"https://yetric.se"}>Yetric AB</a> - Pull Requests Welcome{" "}
-                            <a href={"https://github.com/yetric/corona-2020"}>here</a>
+                            <a href={"https://github.com/CSSEGISandData/COVID-19"}>Data Source</a> - Built by{" "}
+                            <a href={"https://yetric.se"}>Yetric AB</a>
                         </p>
                     </div>
-
-                    <ContextTools />
                 </Analytics>
                 {showModal && (
                     <Modal header={"Cookies and similar"} footer={"Yetric AB 2020"}>
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect } from "react";
 import { observer } from "mobx-react";
-import { GeoOverview } from "../components/GeoOverview";
 
 const Home = observer(() => {
     useEffect(() => {
         document.title = "Corona (Covid-19) Data 2020 - Graph and Table - CoronaData.se";
     });
 
-    return <GeoOverview />;
+    return <div>We have stopped updating the charts since the source is no longer updated.</div>;
 });
 
 export default Home;
